661b055696756c829024b73d642625c19fd876c2
Emre Tümer <emreetumer0@gmail.com>
2025-09-17 19:13:44 +0300

    Car entity için interface ve servisler ayrıca handler metodundaki işlemler yazıldı.

---
+++ CleanArchitecture/src/External/CleanArchitecture.Persistance/Services/CarService.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Persistance.Context;
+
+namespace CleanArchitecture.Persistance.Services;
+public sealed class CarService : ICarService
+{
+    private readonly ApplicationDbContext _context;
+
+    public CarService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(CreateCarCommand request, CancellationToken cancellationToken)
+    {
+        Car car = new()
+        {
+            Name = request.Name,
+            Model = request.Model,
+            EnginePower = request.EnginePower
+        };
+        await _context.Set<Car>().AddAsync(car, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
