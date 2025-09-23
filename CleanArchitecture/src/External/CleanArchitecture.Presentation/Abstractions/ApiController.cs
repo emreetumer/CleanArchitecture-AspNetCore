0bbea77c77b45ce10a2326931fe202187ebca126
Emre Tümer <emreetumer0@gmail.com>
2025-09-23 12:07:56 +0300

    Authorize kontrolleri yapıldı.

--- CleanArchitecture/src/External/CleanArchitecture.Presentation/Abstractions/ApiController.cs
+++ CleanArchitecture/src/External/CleanArchitecture.Presentation/Abstractions/ApiController.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.Presentation.Abstractions;
 [ApiController]
 [Route("api/[controller]")]
+[Authorize(AuthenticationSchemes = "Bearer")]
 public abstract class ApiController : ControllerBase
 {
     public readonly IMediator _mediator;
