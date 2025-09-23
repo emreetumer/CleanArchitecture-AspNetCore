53ca66db3145e7b77573eedfa81724888ecb35cd
Emre Tümer <emreetumer0@gmail.com>
2025-09-23 07:53:26 +0300

    RefreshToken yapısı kuruldu.

--- CleanArchitecture/src/Core/CleanArchitecture.Application/Abstractions/IJwtProvider.cs
+++ CleanArchitecture/src/Core/CleanArchitecture.Application/Abstractions/IJwtProvider.cs
@@ -1,7 +1,8 @@
-﻿using CleanArchitecture.Domain.Entities;
+﻿using CleanArchitecture.Application.Features.AuthFeatures.Commands.Login;
+using CleanArchitecture.Domain.Entities;
 
 namespace CleanArchitecture.Application.Abstractions;
 public interface IJwtProvider
 {
-    string CreateToken(User user);
+    Task<LoginCommandResponse> CreateTokenAsync(User user);
 }
