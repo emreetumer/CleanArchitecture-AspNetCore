88384b24a5ce93db254281642df86e93d7fbfec0
Emre Tümer <emreetumer0@gmail.com>
2025-09-23 09:36:49 +0300

    Refresh Token yapısı kuruldu.

--- CleanArchitecture/src/External/CleanArchitecture.Persistance/Services/AuthService.cs
+++ CleanArchitecture/src/External/CleanArchitecture.Persistance/Services/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CleanArchitecture.Application.Abstractions;
+using CleanArchitecture.Application.Features.AuthFeatures.Commands.CreateNewTokenByRefreshToken;
 using CleanArchitecture.Application.Features.AuthFeatures.Commands.Login;
 using CleanArchitecture.Application.Features.AuthFeatures.Commands.Register;
 using CleanArchitecture.Application.Services;
@@ -20,6 +21,22 @@ public sealed class AuthService : IAuthService
         _jwtProvider = jwtProvider;
     }
 
+    public async Task<LoginCommandResponse> CreateTokenByRefreshTokenAsync(CreateNewTokenByRefreshTokenCommand request, CancellationToken cancellationToken)
+    {
+        User user = await _userManager.FindByIdAsync(request.UserId);
+
+        if (user == null)
+            throw new Exception("Kullanıcı bulunamadı!");
+
+        if (user.RefreshToken != request.RefreshToken)
+            throw new Exception("Refresh token geçerli değil!");
+
+        if (user.RefreshTokenExpires < DateTime.Now)
+            throw new Exception("Refresh tekenun süresi dolmuş!");
+
+        LoginCommandResponse response = await _jwtProvider.CreateTokenAsync(user);
+        return response;
+    }
     public async Task<LoginCommandResponse> LoginAsync(LoginCommand request, CancellationToken cancellationToken)
     {
         User? user = await _userManager.Users.Where(p => p.UserName == request.UserNameOrEmail || p.Email == request.UserNameOrEmail).FirstOrDefaultAsync(cancellationToken);
